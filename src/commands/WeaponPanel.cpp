832f4f1f721e40f0d63e1d8176eeb95ec00f59dd
nizikawa-worms <82811103+nizikawa-worms@users.noreply.github.com>
2021-12-02 08:29:46 +0000

    Update WeaponPanel.cpp
    wrong address in comment

--- src/commands/WeaponPanel.cpp
+++ src/commands/WeaponPanel.cpp
@@ -42,8 +42,8 @@ _asm docall:
 void WeaponPanel::install() {
 	transparencyMode = parseMode(Config::getPanelTransparency());
 	DWORD addrDrawWeaponPanel = _ScanPattern("DrawWeaponPanel", "\x8B\x86\x00\x00\x00\x00\x83\xEC\x10\x85\xC0\x53\x55\x57\x0F\x84\x00\x00\x00\x00\x8B\x5E\x50\x8B\x7E\x4C\x8B\x0E\x8B\x81\x00\x00\x00\x00\x6A\x00\x53\x57\x6A\x00\x50\x8B\x81\x00\x00\x00\x00", "??????xxxxxxxxxx????xxxxxxxxxx????xxxxxxxxx????");
-	// addrDrawWeaponPanel = 005695A2
-	DWORD addrDrawWeaponPanel_patch1 = addrDrawWeaponPanel + 0x22;
+	// addrDrawWeaponPanel = 0x569580
+	DWORD addrDrawWeaponPanel_patch1 = addrDrawWeaponPanel + 0x22; // 0x005695A2
 	addrDrawWeaponPanel_ret = addrDrawWeaponPanel + 0x29;
 	_HookAsm(addrDrawWeaponPanel_patch1, (DWORD) &DrawWeaponPanel_patch1);
 }
